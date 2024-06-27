27646d2ff37ec303bf9d71de0d0e7058c4390949
CoccodrillooXDS <coccodrillo.0001@gmail.com>
2024-06-28 00:07:18 +0200

    Modified initial date (120 days) and min magnitude (5.0)

--- earthquake.py
+++ earthquake.py
@@ -6,7 +6,7 @@ import sys
 from datetime import datetime, timedelta
 from time_related import iso_to_epoch
 
-def download_earthquake_data(initial_date=datetime.now() - timedelta(days=30), end_date=datetime.now()):
+def download_earthquake_data(initial_date=datetime.now() - timedelta(days=120), end_date=datetime.now()):
     if initial_date > end_date:
         print("Initial date must be less than end date")
         sys.exit(1)
@@ -14,7 +14,7 @@ def download_earthquake_data(initial_date=datetime.now() - timedelta(days=30), e
         print("End date must be less than the current date")
         sys.exit(1)
 
-    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={initial_date.date()}%2000:00:00&endtime={end_date.date()}%2023:59:59&minmagnitude=2.5&reviewstatus=reviewed&eventtype=earthquake,accidental%20explosion,acoustic%20noise,acoustic_noise,anthropogenic_event,building%20collapse,chemical%20explosion,chemical_explosion,collapse,debris%20avalanche,eq,experimental%20explosion,explosion,ice%20quake,induced%20or%20triggered%20event,industrial%20explosion,landslide,meteor,meteorite,mine%20collapse,mine_collapse,mining%20explosion,mining_explosion,not%20existing,not%20reported,not_reported,nuclear%20explosion,nuclear_explosion,other,other%20event,other_event,quarry,quarry%20blast,quarry_blast,rock%20burst,Rock%20Slide,rockslide,rock_burst,snow%20avalanche,snow_avalanche,sonic%20boom,sonicboom,sonic_boom,train%20crash,volcanic%20eruption,volcanic%20explosion&orderby=time"
+    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime={initial_date.date()}%2000:00:00&endtime={end_date.date()}%2023:59:59&minmagnitude=5.0&reviewstatus=reviewed&eventtype=earthquake,accidental%20explosion,acoustic%20noise,acoustic_noise,anthropogenic_event,building%20collapse,chemical%20explosion,chemical_explosion,collapse,debris%20avalanche,eq,experimental%20explosion,explosion,ice%20quake,induced%20or%20triggered%20event,industrial%20explosion,landslide,meteor,meteorite,mine%20collapse,mine_collapse,mining%20explosion,mining_explosion,not%20existing,not%20reported,not_reported,nuclear%20explosion,nuclear_explosion,other,other%20event,other_event,quarry,quarry%20blast,quarry_blast,rock%20burst,Rock%20Slide,rockslide,rock_burst,snow%20avalanche,snow_avalanche,sonic%20boom,sonicboom,sonic_boom,train%20crash,volcanic%20eruption,volcanic%20explosion&orderby=time"
     
     response = requests.get(url)
 
