4de9a47bcff54a3a5cfe9e06fbad38588d77cb28
CoccodrillooXDS <coccodrillo.0001@gmail.com>
2024-06-27 15:10:16 +0200

    Uncommented headless argument

--- agile.py
+++ agile.py
@@ -23,7 +23,7 @@ def download_agile_data():
 
     # Set the path to the ChromeDriver executable
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--enable-javascript")
