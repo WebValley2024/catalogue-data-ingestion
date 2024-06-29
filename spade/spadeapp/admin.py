f45d4146bc6b9bc39d86b7c1d497df3ec774a647
unknown <tommaso.scesi@gmail.com>
2024-06-29 10:42:00 +0200

    Added earthquake model

--- spade/spadeapp/admin.py
+++ spade/spadeapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import SampleModel, TGF
+from .models import SampleModel, TGF, Earthquake
 
 # Register your models here.
 admin.site.register(SampleModel)
+admin.site.register(Earthquake)
 admin.site.register(TGF)
--- spade/spadeapp/models.py
+++ spade/spadeapp/models.py
@@ -7,6 +7,28 @@ class SampleModel(models.Model):
     def __str__(self):
         return self.fieldSample
 
+class Earthquake(models.Model):
+    trigger_time = models.DateTimeField("trigger_time")
+    latitude = models.FloatField("latitude")
+    longitude = models.FloatField("longitude")
+    horizontal_err = models.FloatField("horizontal_err")
+    depth = models.FloatField("depth")
+    depth_err = models.FloatField("depth_err")
+    magnitude = models.FloatField("magnitude")
+    magnitude_err = models.FloatField("magnitude_err")
+    magType = models.CharField(max_length=4)
+    nst = models.IntegerField("nst")
+    magnst = models.IntegerField("magnst")
+    gap = models.IntegerField("gap")
+    dmin = models.FloatField("dmin")
+    rms = models.FloatField("rms")
+    net = models.CharField(max_length=2)
+    identifier = models.CharField(max_length=20)
+    place = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
+
+
+
 class TGF(models.Model):
     TGF_name = models.CharField(max_length=30)
     GEO_long = models.FloatField("geo_long")
@@ -27,4 +49,3 @@ class TGF(models.Model):
     end_time_obs = models.DateTimeField("end_time_obs")
     reliability = models.FloatField("reliability")
 
-    
