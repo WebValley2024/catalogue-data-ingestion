93d45a4684497e6e3d5ccb84c70e9e4960e1cf56
unknown <tommaso.scesi@gmail.com>
2024-06-29 11:33:00 +0200

    Added all models

--- spade/spadeapp/admin.py
+++ spade/spadeapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import SampleModel, TGF, Earthquake, SWE
+from .models import SampleModel, TGF, Earthquake, SWE, GRB
 
 # Register your models here.
 admin.site.register(SampleModel)
 admin.site.register(Earthquake)
 admin.site.register(TGF)
 admin.site.register(SWE)
+admin.site.register(GRB)
